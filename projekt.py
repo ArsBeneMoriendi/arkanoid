b83b0a6cf2460c2ecb13191b172ad0bb878df2a4
Zuzia Dybcio <moriendi1422@gmail.com>
2024-11-16 13:29:12 +0100

    lekcja 11

---
+++ projekt.py
@@ -0,0 +1,33 @@
+import pygame
+from Platforma import Platforma
+
+SZEROKOSC_EKRANU = 1024
+WYSOKOSC_EKRANU = 800
+
+ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
+zegar = pygame.time.Clock()
+obraz_tla = pygame.image.load("images/background.png")
+platforma = Platforma()
+
+gra_dziala = True
+while gra_dziala:
+    for zdarzenie in pygame.event.get():
+        if zdarzenie.type == pygame.KEYDOWN:
+            if zdarzenie.key == pygame.K_ESCAPE:
+                gra_dziala = False
+        elif zdarzenie.type == pygame.QUIT:
+            gra_dziala = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        platforma.ruszaj_platforma(-1)
+    if keys[pygame.K_d]:
+        platforma.ruszaj_platforma(1)
+
+
+    ekran.blit(obraz_tla, (0,0))
+    ekran.blit(platforma.obraz_platformy, platforma.pozycja)
+    pygame.display.flip()
+    zegar.tick(30)
+
+pygame.quit()
